a7a6d6014373f58146c0f33307f6b50a8e688855
maycon_v_f <maycon.fedechen@escola.pr.gov.br>
2026-03-05 15:19:33 +0000

    .

--- type-number.js
+++ type-number.js
@@ -3,11 +3,14 @@ const meuNumero = 3;
 const primeiroNumero =1;
 const segundoNumero = 2; 
 
-const operacaoMatematica = primeiroNumero / segundoNumero;
+const operacaoMatematica = primeiroNumero + segundoNumero;
 
 console.log(operacaoMatematica)
 
 //ponto flutuante 
 
 const numeroPontoFlutuante = 3.3;
-const pontoFlutuante
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+console.log (novaOperacao);
